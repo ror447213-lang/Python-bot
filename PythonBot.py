99036676a745f4f5bde414fff30dfbb004269a0c
ror447213-lang <ror447213@gmail.com>
2026-03-23 07:41:18 +0530

    Update PythonBot.py

--- PythonBot.py
+++ PythonBot.py
@@ -1612,4 +1612,4 @@ if __name__ == '__main__':
         except Exception as e:
             print(f"❌ Error: {e}")
             print("🔄 Restarting in 5 seconds...")
-            time.sleep(5)ep(5)
+            time.sleep(5)
