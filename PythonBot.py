5ff24a64258465cb436a935a1a5444050ef7e2ec
ror447213-lang <ror447213@gmail.com>
2026-03-23 07:35:05 +0530

    Update PythonBot.py

--- PythonBot.py
+++ PythonBot.py
@@ -1,3 +1,17 @@
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run_web():
+    app.run(host='0.0.0.0', port=10000)
+
+# Start web server in thread
+threading.Thread(target=run_web).start()
 import os
 import subprocess
 import shutil
